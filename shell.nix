1aebae5492e51e0b31894fb94fc659e17d268642
Orne Brocaar <info@brocaar.com>
2026-07-09 13:14:34 +0100

    chore: update mdbook version

--- shell.nix
+++ shell.nix
@@ -1,12 +1,10 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-26.05.tar.gz") {} }:
 
 pkgs.mkShell {
   buildInputs = [
     pkgs.rustup
     pkgs.mdbook
     pkgs.mdbook-d2
-    pkgs.mdbook-alerts
-    pkgs.mdbook-pagetoc
     pkgs.d2
     pkgs.dprint
   ];
